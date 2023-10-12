7254ad70a240f6066e5407b5d8a254c1abf74951
Devang Chauhan <chauhan.devam@gmail.com>
2023-10-12 12:41:52 +0530

    cards-4: implement a cards micro-service

--- src/main/java/com/easybank/cards/repository/CardRepository.java
+++ src/main/java/com/easybank/cards/repository/CardRepository.java
@@ -1,9 +1,9 @@
 package com.easybank.cards.repository;
 
-import com.easybank.cards.entity.Card;
+import com.easybank.cards.entity.Cards;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CardRepository extends JpaRepository<Card, Long> {
+public interface CardRepository extends JpaRepository<Cards, Long> {
 }
